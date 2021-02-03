c77ce37a1247f1a9ddf90dcadcb89974e73aae36
Samuel Rom <romxx014@umn.edu>
2021-02-03 15:23:06 -0600

    created price2vec file

---
+++ price2vec.py
@@ -0,0 +1,3 @@
+# This is for vectorizing the market's price data.  We can use very small vectors for this and then scale up as necessary.  
+# I'm thinking the daily [high, low, volatility, scale] but we can add anything. 
+
